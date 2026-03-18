ea4b8df963144d5e588b7f821c6a03e4b1d22467
Paul Kim <paul@paulkim.net>
2026-03-17 20:57:21 -0400

    Add theme

--- init.lua
+++ init.lua
@@ -1,4 +1,3 @@
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
@@ -14,6 +13,12 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 local opts = {}
-local plugins = {}
+local plugins = {
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+}
 
 require("lazy").setup(plugins, opts)
+
+-- Theme
+require("catppuccin").setup()
+vim.cmd.colorscheme "catppuccin"
