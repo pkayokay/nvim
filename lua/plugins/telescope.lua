dca0a4c875b9a3381431d40fe0e4aa33f9285fee
Paul Kim <paul@paulkim.net>
2026-08-11 08:10:13 -0400

    Update telescope.lua

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,6 +5,9 @@ return {
     { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
   },
   config = function()
+    -- Registers the compiled fzf-native sorter; without this the build above is unused
+    require('telescope').load_extension('fzf')
+
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<C-p>', builtin.find_files, {})
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
