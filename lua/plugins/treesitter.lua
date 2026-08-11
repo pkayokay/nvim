bd345a04cb2f5147e31e7a23501f7485975c679b
Paul Kim <paul@paulkim.net>
2026-08-11 06:43:48 -0400

    Update treesitter.lua

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     local config = require("nvim-treesitter.configs")
     config.setup({
-      ensure_installed = { "lua", "javascript", "ruby", "elixir" },
+      ensure_installed = { "lua", "javascript", "ruby", "elixir", "typescript", "tsx"},
       highlight = { enable = true },
       indent = { enable = true }
     })
