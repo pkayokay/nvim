38e1457313e8fd9ba2704da2fe06fb1cf63807ed
Paul Kim <paul@paulkim.net>
2026-08-11 11:54:23 -0400

    Set up mason and lsp

---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    lazy = false,
+    opts = {
+      ensure_installed = { "lua_ls", "ruby_lsp" },
+    }
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+    end
+  }
+}
