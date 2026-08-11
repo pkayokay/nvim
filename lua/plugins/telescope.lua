bcbff39992bc2a3fbfef09f67f99bda37f396c45
Paul Kim <paul@paulkim.net>
2026-08-11 12:17:17 -0400

    Add telescope-ui-select for vim.ui.select prompts

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,10 +3,19 @@ return {
   dependencies = {
     'nvim-lua/plenary.nvim',
     { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    'nvim-telescope/telescope-ui-select.nvim',
   },
   config = function()
+    require('telescope').setup({
+      extensions = {
+        ['ui-select'] = require('telescope.themes').get_dropdown({}),
+      },
+    })
+
     -- Registers the compiled fzf-native sorter; without this the build above is unused
     require('telescope').load_extension('fzf')
+    -- Routes vim.ui.select (LSP code actions, etc.) through telescope
+    require('telescope').load_extension('ui-select')
 
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<C-p>', builtin.find_files, {})
