891be6c024a32832239fb3ddc625310e439aa409
Paul Kim <paul@paulkim.net>
2026-08-11 07:23:18 -0400

    Add vim options file for binds

--- init.lua
+++ init.lua
@@ -1,10 +1,3 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=2")
-vim.cmd("set softtabstop=2")
-vim.cmd("set shiftwidth=2")
-
-vim.g.mapleader = " "
-
 -- Lazy Vim: Package Manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
