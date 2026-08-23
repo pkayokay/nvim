b0ab1eab5d2ec54ef5b0557f87d8f0507749c735
Paul Kim <paul@paulkim.net>
2026-08-23 07:38:17 -0400

    Port leader and indent settings to vim.opt

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,14 +1,15 @@
 -- Core editor settings. Loaded from init.lua BEFORE any plugin, because mapleader
 -- has to exist before the plugin files register their <leader> mappings.
 
-vim.cmd("set expandtab")     -- pressing Tab inserts spaces, never a literal tab character
-vim.cmd("set tabstop=2")     -- an existing tab character renders 2 columns wide
-vim.cmd("set softtabstop=2") -- Tab/Backspace move by 2 spaces, so they feel like one unit
-vim.cmd("set shiftwidth=2")  -- auto-indent and the >> / << commands shift by 2
-
 -- <leader> is a prefix key you press before a shortcut. Space is the convention.
 -- Every "<leader>x" mapping in lua/plugins/ therefore means: press Space, then x.
 vim.g.mapleader = " "
+
+-- Indentation
+vim.opt.expandtab = true     -- pressing Tab inserts spaces, never a literal tab character
+vim.opt.tabstop = 2          -- an existing tab character renders 2 columns wide
+vim.opt.softtabstop = 2      -- Tab/Backspace move by 2 spaces, so they feel like one unit
+vim.opt.shiftwidth = 2       -- auto-indent and the >> / << commands shift by 2
 
 -- Print the diagnostic message inline, to the right of the offending line.
 -- Neovim 0.11+ ships this off by default: errors show only as a gutter sign,
